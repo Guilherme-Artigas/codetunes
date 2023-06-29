06efde1e1c097d7b19ed5a3451498197f7633cde
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-06-29 08:52:08 -0300

    feat: implementação do componente Loading

---
+++ src/frontend/components/Loading.tsx
@@ -0,0 +1,15 @@
+export default function Loading() {
+
+  return (
+    <div
+      className={`
+        animate-spin
+        border-4
+        border-t-[#003BE5]
+        h-10
+        w-10
+        rounded-full
+      `}
+    />
+  );
+}
