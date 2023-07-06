15c990a768f5e90897f8903adf8495cdcf762560
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-07-06 14:13:16 -0300

    refactor: adicionando método na chamada a API

--- src/frontend/utils/requestMusics.ts
+++ src/frontend/utils/requestMusics.ts
@@ -2,7 +2,7 @@ import IMusic from '../interfaces/IMusic';
 
 export default async function requestMusic(id: string): Promise<IMusic[]> {
   // eslint-disable-next-line max-len
-  const response = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song&country=no`);
+  const response = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song&country=no`, { method: 'GET' });
   const { results } = await response.json();
 
   return results;
