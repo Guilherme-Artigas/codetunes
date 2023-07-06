7c6632dc37fffa3b36eebe451dcfa7ccf5f22658
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-07-06 11:51:20 -0300

    chore: comentando funções que não estão sem utilizadas

--- src/backend/services/index.ts
+++ src/backend/services/index.ts
@@ -1,17 +1,19 @@
 import {
   createUser,
-  deleteUser,
-  getAllUsers,
-  getOneUser,
-  updateUser
+  // deleteUser,
+  // getAllUsers,
+  // getOneUser,
+  loginUser
+  // updateUser
 } from '@/backend/services/User.service';
 
 const userService = {
   createUser,
-  deleteUser,
-  getAllUsers,
-  getOneUser,
-  updateUser
+  // deleteUser,
+  // getAllUsers,
+  // getOneUser,
+  // updateUser
+  loginUser
 };
 
 export default userService;
