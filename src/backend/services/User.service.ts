7bd788d3e39eb958f24fc990e937d74566c53a36
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-07-06 11:51:58 -0300

    feat: implementação do cadastro e do login de usuários

--- src/backend/services/User.service.ts
+++ src/backend/services/User.service.ts
@@ -1,24 +1,25 @@
 import IUserProfile from '@/backend/interfaces/IUserProfile';
+import loginUserSchema from './validations/LoginUser';
 import userModel from '@/backend/models';
 import userSchema from './validations/NewUser';
 
-export async function getAllUsers() {
-  const result = await userModel.getAllUsers();
+// export async function getAllUsers() {
+//   const result = await userModel.getAllUsers();
 
-  return { status: 200, result };
-}
+//   return { status: 200, result };
+// }
 
-export async function getOneUser(userName: string) {
-  const result = await userModel.getOneUser(userName);
-  if (!result) {
-    return {
-      status: 404,
-      message: `Usuário ${userName} não encontrado.`
-    };
-  }
+// export async function getOneUser(userName: string) {
+//   const result = await userModel.getOneUser(userName);
+//   if (!result) {
+//     return {
+//       status: 404,
+//       message: `Usuário ${userName} não encontrado.`
+//     };
+//   }
 
-  return { status: 200, message: result };
-}
+//   return { status: 200, message: result };
+// }
 
 export async function createUser(user: IUserProfile) {
   const { error } = userSchema.validate(user);
@@ -29,20 +30,38 @@ export async function createUser(user: IUserProfile) {
   return { status: 201, message: 'Usuário cadastrado com sucesso!' };
 }
 
-export async function updateUser(nome: string, user: IUserProfile) {
-  const { error } = userSchema.validate(user);
+// export async function updateUser(nome: string, user: IUserProfile) {
+//   const { error } = userSchema.validate(user);
+//   if (error) return { status: 400, message: error.message };
+
+//   await userModel.updateUser(nome, user);
+
+//   return { status: 200, message: 'Usuário atualizado com sucesso!' };
+// }
+
+// export async function deleteUser(nome: string) {
+//   const { status, message } = await getOneUser(nome);
+
+//   if (status === 404) return { status, message };
+//   await userModel.deleteUser(nome);
+
+//   return { status: 200, message: 'Usuário excluído com sucesso!' };
+// }
+
+export async function loginUser(userEmail: string, userPass: string) {
+  const { error } = loginUserSchema.validate({ userEmail, userPass });
   if (error) return { status: 400, message: error.message };
 
-  await userModel.updateUser(nome, user);
+  const user = await userModel.getOneUser(userEmail, userPass);
 
-  return { status: 200, message: 'Usuário atualizado com sucesso!' };
-}
+  if (!user) return { status: 404, message: 'Email ou senha inválidos' };
 
-export async function deleteUser(nome: string) {
-  const { status, message } = await getOneUser(nome);
-
-  if (status === 404) return { status, message };
-  await userModel.deleteUser(nome);
-
-  return { status: 200, message: 'Usuário excluído com sucesso!' };
+  return {
+    status: 200,
+    message: {
+      userName: user.userName,
+      userEmail: user.userEmail,
+      userImg: user.userImg
+    }
+  };
 }
