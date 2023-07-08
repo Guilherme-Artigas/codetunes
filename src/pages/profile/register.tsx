cb6d2fa3268f29b9317ce60fa1e935407f6297e3
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-07-07 21:40:25 -0300

    muda nome do botão de cadastro

--- src/pages/profile/register.tsx
+++ src/pages/profile/register.tsx
@@ -119,7 +119,7 @@ export default function Register() {
               disabled={booleanButton}
               onClick={register}
             >
-              Cadastro
+              Cadastrar
             </button>
 
           </form>
